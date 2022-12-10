946cb4977f62b6d07bbdb73733735ff225810ad7
Emil Makaev <onmakaev@gmail.com>
2022-12-10 17:42:10 +0600

    first commit

---
+++ src/index.tsx
@@ -0,0 +1,7 @@
+import * as React from 'react';
+
+// Delete me
+export const Thing = () => {
+  console.log('the snozzberries taste like snozzberries');
+  return <div>the snozzberries taste like snozzberries</div>;
+};
